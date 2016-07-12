7df1dc207014a73e2b5b3f5c8ea3d5fe9737af83
Ricardo Machado <pt.n00b@gmail.com>
2016-07-12 17:50:39 +0200

    Improved the spec to provide more information about the createComponent's behavior (#6)

--- test/createComponent.spec.js
+++ test/createComponent.spec.js
@@ -6,27 +6,27 @@ import sinonChai from 'sinon-chai';
 import createComponent from '../src/createComponent';
 
 const sandbox = sinon.sandbox.create();
-
 chai.use(sinonChai);
 
-
 describe('createComponent', function () {
+  const mySpec = {
+    myCustomFunction() { return 'foo'; },
+    render() { return null; }
+  };
+  let MyComponent;
+  let myComponentInstance;
+
   beforeEach(function () {
     sandbox.spy(React, 'createClass');
+    MyComponent = createComponent(mySpec);
+    myComponentInstance = new MyComponent();
   });
 
   afterEach(function () {
     sandbox.restore();
   });
 
-  it('creates a component', function () {
-    const mySpec = {
-      myCustomFunction() { return 'foo'; },
-      render() { return null; }
-    };
-    const MyComponent = createComponent(mySpec);
-    const myComponentInstance = new MyComponent();
-
+  it('calls React.createClass once, at component creation', function () {
     expect(React.createClass)
       .to.be.callCount(1)
       .and.to.be.calledWith({
@@ -35,8 +35,14 @@ describe('createComponent', function () {
         componentDidMount: sinon.match.func,
         componentWillUnmount: sinon.match.func
       });
-    expect(myComponentInstance).to.be.instanceof(MyComponent);
+  });
+
+  it('is a valid React component and a MyComponent\'s instance', function () {
     expect('isReactComponent' in Object.getPrototypeOf(myComponentInstance)).to.be.equal(true);
+    expect(myComponentInstance).to.be.instanceof(MyComponent);
+  });
+
+  it('has the spec\'s functions', function () {
     expect(myComponentInstance.myCustomFunction()).to.be.equal('foo');
     expect(myComponentInstance.render()).to.be.equal(null);
   });

8c45e89003fe8a707c524919f7d97273d204743b
Ricardo Machado <rmachado@travix.com>
2016-07-12 14:01:43 +0200

    Improved the test to provide a more readable explanation of how the code works

--- test/createFactory.spec.js
+++ test/createFactory.spec.js
@@ -6,24 +6,25 @@ import createFactory from '../src/createFactory';
 chai.use(sinonChai);
 
 describe('createFactory', function () {
-  it('returns a Factory class', function () {
-    const fakeAppInstance = {};
-    const mySpec = {
-      initialize: sinon.stub(),
-      myCustomFunction: () => 'value'
-    };
+  const fakeAppInstance = {};
+  const mySpec = {
+    initialize: sinon.stub(),
+    myCustomFunction: () => 'value'
+  };
 
-    const MyFactory = createFactory(mySpec);
-    const myFactoryInstance = new MyFactory({ app: fakeAppInstance });
+  const MyFactory = createFactory(mySpec);
+  const myFactoryInstance = new MyFactory({ app: fakeAppInstance });
 
-    /** Executes the initialize() at construction */
+  it('executes the initialize() at construction', function () {
     expect(mySpec.initialize).to.be.callCount(1);
+  });
 
-    /** Must be an instance of MyFactory and contain the app instance passed */
+  it('must be an instance of MyFactory and have the app passed', function () {
     expect(myFactoryInstance).to.be.instanceOf(MyFactory);
     expect(myFactoryInstance.app).to.be.deep.equal(fakeAppInstance);
+  });
 
-    /* And contain the functions passed in the spec */
+  it('must contain the functions passed in the spec', function () {
     expect('myCustomFunction' in myFactoryInstance).to.be.equal(true);
     expect(myFactoryInstance.myCustomFunction()).to.be.equal('value');
   });

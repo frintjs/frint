92aa5138326c285443d648ab94cf94bbde8ff4e2
Ricardo Machado <rmachado@travix.com>
2016-07-15 21:54:45 +0200

    Added a 'root' element to render components on a specific element instead of using the body

--- test/setup.js
+++ test/setup.js
@@ -1,6 +1,6 @@
 import { jsdom } from 'jsdom';
 
-global.document = jsdom('<html><body></body></html>');
+global.document = jsdom('<html><body><div id="root"></div></body></html>');
 global.window = global.document.defaultView;
 global.location = global.window.location;
 global.navigator = { userAgent: 'node.js' };

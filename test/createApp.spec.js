b0cb6b1b644529043031069f6c927a436b4a24d1
Ricardo Machado <rmachado@travix.com>
2016-08-16 12:34:04 +0200

    Added extra verification to check if it returns undefined when the fallback fails

--- test/createApp.spec.js
+++ test/createApp.spec.js
@@ -4,6 +4,14 @@ import { expect } from 'chai';
 import createApp from '../src/createApp';
 
 describe('createApp', () => {
+  beforeEach(() => {
+    window.app = null;
+  });
+
+  afterEach(() => {
+    delete window.app;
+  });
+
   it('creates an instance', () => {
     const App = createApp({
       name: 'MyAppName',
@@ -33,7 +41,6 @@ describe('createApp', () => {
     expect(app.getValidationFunctions()).to.eql(expectedValidationFunctions);
   });
 
-
   it('creates a RootApp instance with a validationFunctions option and a ChildApp returns them via getValidationFunctions()', () => {
     const expectedValidationFunctions = ['expectedValue'];
     const unExpectedValidationFunctions = ['unExpectedValue'];
@@ -62,6 +69,32 @@ describe('createApp', () => {
 
     expect(rootApp.getValidationFunctions()).to.eql(expectedValidationFunctions);
     expect(childApp.getValidationFunctions()).to.eql(expectedValidationFunctions);
+  });
+
+  it('creates a RootApp instance without a validationFunctions option and so as the ChildApp. Expects undefined when calling getValidationFunctions()', () => {
+    const RootApp = createApp({
+      appId: '123',
+      component: true,
+      name: 'MyRootAppName',
+    });
+    const rootApp = new RootApp();
+    window.app = rootApp;
+
+    const ChildApp = createApp({
+      appId: '234',
+      component: true,
+      name: 'MyChildAppName',
+    });
+    const childApp = new ChildApp();
+
+
+    expect(rootApp).to.be.instanceof(RootApp);
+    expect(window.app).to.eql(rootApp);
+
+    expect(childApp).to.be.instanceof(ChildApp);
+
+    expect(rootApp.getValidationFunctions()).to.eql(undefined);
+    expect(childApp.getValidationFunctions()).to.eql(undefined);
 
     delete window.app;
   });

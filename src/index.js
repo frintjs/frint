3468ffe1f15522316fc43fe7df28bf2aabed8722
Alex Miranda <alexmirandamoraes@gmail.com>
2016-09-05 13:03:45 +0200

    Fix mixed ES syntax

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import { combineReducers } from 'redux';
+
+import createApp from './createApp';
+import createComponent from './createComponent';
+import createFactory from './createFactory';
+import createModel from './createModel';
+import createService from './createService';
+import mapToProps from './components/mapToProps';
+import Model from './Model';
+import PropTypes from './PropTypes';
+import Region from './components/Region';
+import render from './render';
+
+export default {
+  combineReducers,
+  createApp,
+  createComponent,
+  createFactory,
+  createModel,
+  createService,
+  mapToProps,
+  Model,
+  PropTypes,
+  Region,
+  render,
+};

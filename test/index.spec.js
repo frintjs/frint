8ea2548ccc52fe3b6a8e13ae30324524cefc3d35
Alex Miranda <alexmirandamoraes@gmail.com>
2016-09-05 16:34:40 +0200

    Add unit tests for index.js

---
+++ test/index.spec.js
@@ -0,0 +1,27 @@
+/* global describe, it */
+/* eslint-disable global-require */
+
+import _ from 'lodash';
+import { expect } from 'chai';
+
+import frint from '../src';
+
+describe('index', function () {
+  const publicModules = {
+    combineReducers: require('redux').combineReducers,
+    createApp: require('../src/createApp'),
+    createComponent: require('../src/createComponent'),
+    createFactory: require('../src/createFactory'),
+    createModel: require('../src/createModel'),
+    Model: require('../src/Model'),
+    PropTypes: require('../src/PropTypes'),
+    Region: require('../src/components/Region'),
+    render: require('../src/render'),
+  };
+
+  _.each(publicModules, (module, moduleName) => {
+    it(`exports module ${moduleName}`, () => {
+      expect(frint[moduleName]).to.be.eql(module);
+    });
+  });
+});

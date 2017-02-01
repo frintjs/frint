147fe3e29612c72db4b0d32024e0ddf1fa70c47b
Fahad Ibnay Heylaal <fahad19@gmail.com>
2017-02-01 11:07:32 +0100

    examples: kitchensink

---
+++ examples/kitchensink/widget-color/constants/index.js
@@ -0,0 +1,5 @@
+export const GREEN_COLOR = '#5cb85c';
+export const RED_COLOR = '#d9534f';
+export const DEFAULT_COLOR = GREEN_COLOR;
+
+export const CHANGE_COLOR = 'CHANGE_COLOR';
---
+++ examples/kitchensink/widget-counter/index.js
@@ -0,0 +1,8 @@
+import App from './app';
+
+const app = new App();
+
+app.readStateFrom(['WidgetColor']);
+app.setRegion('main');
+
+export default app;

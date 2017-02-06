60c1e462f1181aafb6adbbecf85ec27fad308d9c
Fahad Ibnay Heylaal <fahad19@gmail.com>
2017-02-06 16:36:49 +0100

    kitchensink: color widget

--- examples/kitchensink/widget-color/app/index.js
+++ examples/kitchensink/widget-color/app/index.js
@@ -1,10 +1,35 @@
-import { createApp } from 'frint';
+import { createApp, createStore, RegionService } from 'frint';
 
+import DEFAULT_COLOR from '../constants';
 import RootComponent from '../components/Root';
 import rootReducer from '../reducers';
 
 export default createApp({
   name: 'WidgetColor',
-  component: RootComponent,
-  reducer: rootReducer,
+  providers: [
+    {
+      name: 'component',
+      useValue: RootComponent,
+    },
+    {
+      name: 'store',
+      useFactory: (app) => {
+        const Store = createStore({
+          initialState: {
+            color: {
+              value: DEFAULT_COLOR,
+            },
+          },
+          reducer: rootReducer,
+          thunkArgument: { app },
+        });
+
+        return new Store();
+      },
+    },
+    {
+      name: 'region',
+      useClass: RegionService,
+    }
+  ],
 });

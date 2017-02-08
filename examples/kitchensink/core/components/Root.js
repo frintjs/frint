0f0a0326d09a610b1f464cf4bc004cb937de2b56
Fahad Ibnay Heylaal <fahad19@gmail.com>
2017-02-08 14:48:55 +0100

    sidebar toggle example

--- examples/kitchensink/core/components/Root.js
+++ examples/kitchensink/core/components/Root.js
@@ -1,6 +1,7 @@
-import { createComponent, Region } from 'frint';
+import { createComponent, Region, observe } from 'frint';
+import { Observable, BehaviorSubject } from 'rxjs';
 
-export default createComponent({
+const Root = createComponent({
   render() {
     return (
       <div className="container">
@@ -10,18 +11,54 @@ export default createComponent({
 
             <hr />
 
-            <Region name="main" data={{hi: `available from props of 'main' region`}} />
+            <Region
+              name="main"
+              data={{hi: `available from props of 'main' region`}}
+            />
+
+            <a
+              href="#"
+              onClick={() => this.props.toggle(!this.props.showSidebar)}
+            >
+              Toggle
+            </a>
           </div>
 
-          <div className="four columns">
-            <h3>Sidebar</h3>
+          {this.props.showSidebar && (
+            <div className="four columns">
+              <h3>Sidebar</h3>
 
-            <hr />
+              <hr />
 
-            <Region name="sidebar" data={{hi: `data from 'sidebar' region here`}} />
-          </div>
+              <Region name="sidebar" data={{hi: `data from 'sidebar' region here`}} />
+            </div>
+          )}
         </div>
       </div>
     );
   }
 });
+
+export default observe(function (app) {
+  const sidebarToggle$ = (new BehaviorSubject(true))
+    .map((toggleValue) => {
+      return {
+        showSidebar: toggleValue,
+      };
+    });
+
+  const actions$ = Observable.of({
+    toggle: (value) => {
+      sidebarToggle$.next(value);
+    }
+  });
+
+  return sidebarToggle$
+    .merge(actions$)
+    .scan((props, emitted) => {
+      return {
+        ...props,
+        ...emitted,
+      };
+    }, {});
+})(Root);

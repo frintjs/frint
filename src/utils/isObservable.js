5e05682105e8c631ec142d6eb35e24caf5b546f4
Fahad Ibnay Heylaal <fahad19@gmail.com>
2017-02-12 02:01:09 +0100

    fix in example for implementing `streamProps()`.

--- src/utils/isObservable.js
+++ src/utils/isObservable.js
@@ -1,11 +1,18 @@
 /**
  * Check if given `obj` is an Observable or not.
  *
+ * @TODO: this function needs to be more proper and reliable
+ *
  * @param any obj
  * @return boolean
  */
 export default function isObservable(obj) {
-  if (obj && typeof obj.subscribe === 'function') {
+  if (
+    obj &&
+    typeof obj.subscribe === 'function' &&
+    typeof obj.map === 'function' &&
+    typeof obj.filter === 'function'
+  ) {
     return true;
   }
 

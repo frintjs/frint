43ea6ccad15708ccce24a2e8e7ce687667fd3843
Fahad Ibnay Heylaal <fahad19@gmail.com>
2017-02-14 15:49:13 +0100

    examples: in-place editor for todo items.

--- examples/kitchensink/widget-todos/components/Item.js
+++ examples/kitchensink/widget-todos/components/Item.js
@@ -1,6 +1,7 @@
 import { h, createComponent, observe, streamProps, Region } from 'frint';
+import { BehaviorSubject } from 'rxjs';
 
-import { removeTodo } from '../actions/todos';
+import { removeTodo, updateTodo } from '../actions/todos';
 
 const Item = createComponent({
   render() {
@@ -8,7 +9,37 @@ const Item = createComponent({
 
     return (
       <div style={{background: '#f1f1f1', border: '1px solid #e1e1e1', marginBottom: '15px', padding: '15px', borderRadius: '4px'}}>
-        <p>{todo.title} [<a href="#" onClick={() => this.props.removeTodo(todo.id)}>x</a>]</p>
+        {!this.props.showEditForm && (
+          <p>
+            {todo.title}
+
+            [<a href="javascript:" onClick={() => this.props.edit(todo)}>edit</a>]
+            [<a href="javascript:" onClick={() => this.props.removeTodo(todo.id)}>x</a>]
+          </p>
+        )}
+
+        {this.props.showEditForm && (
+          <p>
+            <input
+              class="u-full-width"
+              type="text"
+              placeholder="my todo title..."
+              id="todoItemInput"
+              value={this.props.inputValue}
+              onChange={(e) => this.props.changeInput(e.target.value)}
+            />
+
+            <button
+              type="button"
+              className="button-primary"
+              onClick={() => this.props.submit(todo.id, this.props.inputValue)}
+            >
+              Submit
+            </button>
+
+            [<a href="javascript:" onClick={() => this.props.cancelEdit()}>cancel</a>]
+          </p>
+        )}
 
         <p>Below is Region <strong>todo-item</strong> that is specific to this Todo item:</p>
 
@@ -23,12 +54,47 @@ const Item = createComponent({
 });
 
 export default observe(function (app) {
+  const showEditForm$ = new BehaviorSubject(false); // start with hidden form
+  const formInput$ = new BehaviorSubject('');
+  const store = app.get('store');
+
+  const cancelEdit = () => {
+    formInput$.next(''); // clear input field value
+    showEditForm$.next(false);
+  };
+
   return streamProps()
-    // actions
+    // dispatchable actions against store
     .setDispatch(
       { removeTodo },
-      app.get('store')
+      store
     )
+
+    // stream values
+    .set(
+      showEditForm$,
+      (showEditForm) => ({ showEditForm })
+    )
+    .set(
+      formInput$,
+      (inputValue) => ({ inputValue })
+    )
+
+    // form actions
+    .set({
+      edit: (todo) => {
+        formInput$.next(todo.title); // set input field value
+        showEditForm$.next(true);
+      },
+      changeInput: (value) => {
+        formInput$.next(value);
+      },
+      cancelEdit,
+      submit: (id, newTitle) => {
+        store.dispatch(updateTodo(id, newTitle));
+        cancelEdit();
+      }
+    })
 
     // final observable
     .get$();

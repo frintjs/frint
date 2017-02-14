01e0f113aaa83058ce737f11d58d94b30ca5e82c
Fahad Ibnay Heylaal <fahad19@gmail.com>
2017-02-14 18:01:33 +0100

    backwards compatibility: createApp’s component/services/factories/models/store.

--- src/createService.js
+++ src/createService.js
@@ -1,6 +1,8 @@
 import _ from 'lodash';
 
 export default function createService(extend = {}) {
+  console.warn('[DEPRECATED] `createService` has been deprecated. Use `createClass` or direct ES6 classes instead.');
+
   class Service {
     constructor(options = {}) {
       if (!options.app) {

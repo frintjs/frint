b7bc340bb0c5da32af311010037b7430ef3dd9c0
Fahad Ibnay Heylaal <fahad19@gmail.com>
2017-02-20 18:11:23 +0100

    tests: full coverage in `react`.

--- src/react/test/isObservable.spec.js
+++ src/react/test/isObservable.spec.js
@@ -2,7 +2,7 @@
 import { expect } from 'chai';
 import { Observable } from 'rxjs';
 
-import { isObservable } from './Frint';
+import isObservable from '../isObservable';
 
 describe('react › isObservable', function () {
   it('returns true when an Observable is given', function () {

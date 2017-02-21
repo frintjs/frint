c805207efa87cc43c158244cfdf8b886095567b0
Fahad Ibnay Heylaal <fahad19@gmail.com>
2017-02-21 10:41:29 +0100

    tests: base specs in `react` for getting true coverage report.

---
+++ src/react/test/components/observe.spec.js
@@ -0,0 +1,10 @@
+/* global describe, it */
+import { expect } from 'chai';
+
+import observe from '../../components/observe';
+
+describe('react › components › observe', function () {
+  it('is a function', function () {
+    expect(observe).to.be.a('function');
+  });
+});
---
+++ src/react/test/services/Region.spec.js
@@ -0,0 +1,10 @@
+/* global describe, it */
+import { expect } from 'chai';
+
+import RegionService from '../../services/Region';
+
+describe('react › services › Region', function () {
+  it('is a function', function () {
+    expect(RegionService).to.be.a('function');
+  });
+});

7159629d3318b954c74aae39b0e062b1b54e6dab
Alex Miranda <alexmirandamoraes@gmail.com>
2017-03-01 10:39:31 +0100

    Extract lifecycle hooks to react package (#91)
    * Extract lifecycle hooks to react package
    
    * Remove tests moved to react/test/extendApp.spec.js
    
    * Fix lint errors

--- src/react/extendApp.js
+++ src/react/extendApp.js
@@ -12,4 +12,15 @@ export default function extendApp(App) {
       </Provider>
     );
   };
+
+  ['beforeMount', 'afterMount', 'beforeUnmount'].forEach((hookName) => {
+    App.prototype[hookName] = function lifecycleHook(...args) {
+      const { [hookName]: hook = function noop() {} } = this.options;
+      try {
+        return hook.apply(this, args);
+      } finally {
+        this[hookName] = hook;
+      }
+    };
+  });
 }

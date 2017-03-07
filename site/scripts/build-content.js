6c10670278ad0265200ccdbc9b93968718a5021f
Fahad Ibnay Heylaal <fahad19@gmail.com>
2017-03-07 16:36:36 +0100

    process markdown.

--- site/scripts/build-content.js
+++ site/scripts/build-content.js
@@ -1,18 +1,47 @@
+const _ = require('lodash');
 const Metalsmith = require('metalsmith');
 const marked = require('marked');
 
 Metalsmith(__dirname)
   .source(__dirname + '/../content')
-  .destination(__dirname + '/../../site')
+  .destination(__dirname + '/../../_site')
+
+  // buffer to string - please don't judge me
+  .use(function (files, metalsmith, done) {
+    _.each(files, function (obj, file) {
+      files[file].contents = obj.contents.toString();
+    });
+    done();
+  })
 
   // markdown
-  .use(function convertMarkdown() {
-    return function plugin(files, metalsmith, done) {
-      setImmediate(done);
-      Object.keys(files).forEach(function (file) {
-        console.log('file', file);
-      });
-    };
+  .use(function convertMarkdown(files, metalsmith, done) {
+    Object.keys(files).forEach(function (file) {
+      files[file].contents = marked(files[file].contents);
+    });
+
+    done();
+  })
+
+  // url - filepath
+  .use(function urls(files, metalsmith, done) {
+    Object.keys(files).forEach(function (file) {
+      console.log('file', file);
+
+      const content = files[file];
+      files[file.replace('.md', '.html')] = content;
+      delete files[file];
+    });
+
+    done();
+  })
+
+  // string to buffer - please don't judge me
+  .use(function (files, metalsmith, done) {
+    _.each(files, function (obj, file) {
+      files[file].contents = new Buffer(obj.contents);
+    });
+    done();
   })
 
   // build

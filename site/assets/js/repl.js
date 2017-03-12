062b5398606dbb8f15c2e31e1e7d0ddfff950402
Fahad Ibnay Heylaal <fahad19@gmail.com>
2017-03-12 21:57:12 +0100

    shareable code via URLs.

--- site/assets/js/repl.js
+++ site/assets/js/repl.js
@@ -24,14 +24,10 @@ const App = createApp({
 const app = new App();
 render(app, document.getElementById('root'));`;
 
+// editor
 const editor = ace.edit('editor');
 editor.setTheme('ace/theme/chrome');
 editor.getSession().setMode('ace/mode/javascript');
-editor.setValue(initialCode);
-
-editor.getSession().on('change', function (e) {
-  renderToRoot();
-});
 
 function renderToRoot() {
   ReactDOM.unmountComponentAtNode(document.getElementById('root'));
@@ -45,6 +41,60 @@ function renderToRoot() {
   }).code;
 
   eval(output);
+  updateUrlHash(input);
 }
 
-renderToRoot();
+function parseHashParams(hash) {
+  const params = hash
+    .split('#?')[1];
+
+  if (!params) {
+    return {};
+  }
+
+  return params
+    .split('&')
+    .map(function (item) {
+      const kv = item.split('=');
+      const key = kv[0];
+      const value = kv[1];
+      return { [key]: value };
+    })
+    .reduce(function (acc, val) {
+      return Object.assign({}, acc, val);
+    }, {});
+}
+
+function updateUrlHash(code = null) {
+  const currentHashParams= parseHashParams(location.hash);
+  currentHashParams.code = escape(code);
+
+  const stringifiedParams = Object.keys(currentHashParams)
+    .map(function (key) {
+      return key + '=' + currentHashParams[key];
+    })
+    .join('&');
+
+  if (history.pushState) {
+    history.pushState(null, null, '#?' + stringifiedParams);
+  }
+}
+
+
+(function () {
+  // get code from URL on first page-load
+  const parsedParams = parseHashParams(location.hash);
+  if (typeof parsedParams.code !== 'undefined') {
+    editor.setValue(unescape(parsedParams.code));
+  } else {
+    editor.setValue(initialCode);
+  }
+
+  // re-render on changes
+  editor.getSession().on('change', function (e) {
+    renderToRoot();
+  });
+
+  // initial render
+  renderToRoot();
+})();

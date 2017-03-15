238e03db3443cc89806fe959625ce27a371bc000
Fahad Ibnay Heylaal <fahad19@gmail.com>
2017-03-15 11:40:30 +0100

    Test: Server-side rendering with props coming from Observable (#128)
    * tests: for server-side rendering with observed props.
    
    * rxjs as a devDependency.
    
    * eslint fixes.
    
    * casing -_-

--- packages/frint-react-server/src/renderToString.spec.js
+++ packages/frint-react-server/src/renderToString.spec.js
@@ -1,9 +1,11 @@
-/* eslint-disable import/no-extraneous-dependencies, func-names */
+/* eslint-disable import/no-extraneous-dependencies, func-names, react/prop-types */
 /* global describe, it */
 import React from 'react';
+import { Observable } from 'rxjs';
 import { expect } from 'chai';
 
 import { createCore } from 'frint';
+import { observe } from 'frint-react';
 
 import renderToString from './renderToString';
 
@@ -37,5 +39,38 @@ describe('frint-react-server › renderToString', function () {
 
     const html = renderToString(app);
     expect(html).to.contain('>Hello World!</p></div>');
+  });
+
+  it('returns HTML output of an App instance, with observed props', function () {
+    const TestComponent = React.createClass({
+      render() {
+        return (
+          <div>
+            <p>{this.props.name}</p>
+          </div>
+        );
+      }
+    });
+
+    const ObservedTestComponent = observe(function (app) {
+      return Observable.of({
+        name: app.getOption('name'),
+      });
+    })(TestComponent);
+
+    const TestApp = createCore({
+      name: 'TestAppName',
+      providers: [
+        {
+          name: 'component',
+          useValue: ObservedTestComponent,
+        },
+      ],
+    });
+
+    const app = new TestApp();
+
+    const html = renderToString(app);
+    expect(html).to.contain('>TestAppName</p></div>');
   });
 });

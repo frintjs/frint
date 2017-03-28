cf1fb4fcdb3e2bbe58fc46d37822442e1998fa3e
Fahad Ibnay Heylaal <fahad19@gmail.com>
2017-03-28 21:45:05 +0200

    frint-router: init.

---
+++ packages/frint-react-router/README.md
@@ -0,0 +1,16 @@
+# frint-react-router
+
+[![npm](https://img.shields.io/npm/v/frint-react-router.svg)](https://www.npmjs.com/package/frint-react-router)
+
+> React Router package for Frint
+
+<!-- MarkdownTOC autolink=true bracket=round -->
+
+- [Guide](#guide)
+- [API](#api)
+
+<!-- /MarkdownTOC -->
+
+# Guide
+
+# API
---
+++ packages/frint-react-router/webpack.config.js
@@ -0,0 +1,49 @@
+var webpack = require('webpack');
+
+var minify = process.env.DIST_MIN;
+var plugins = !minify
+  ? []
+  : [
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false,
+        drop_console: false
+      }
+    })
+  ];
+var filename = !minify
+  ? 'frint-react-router.js'
+  : 'frint-react-router.min.js';
+
+module.exports = {
+  entry: __dirname + '/src',
+  output: {
+    path: __dirname + '/dist',
+    filename: filename,
+    libraryTarget: 'this',
+    library: 'FrintReactRouter'
+  },
+  externals: {
+    'frint': 'Frint',
+    'lodash': '_',
+    'react': 'React',
+    'react-dom': 'ReactDOM',
+    'rxjs': 'Rx',
+  },
+  target: 'web',
+  plugins: plugins,
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        loader: 'babel-loader',
+        query: {
+          presets: [
+            'travix'
+          ]
+        }
+      }
+    ]
+  }
+};

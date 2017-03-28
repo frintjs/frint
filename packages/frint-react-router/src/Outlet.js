8a3563ecdcab7d620982d989c048b683a47b9654
Fahad Ibnay Heylaal <fahad19@gmail.com>
2017-03-28 23:28:10 +0200

    base for Outlet.

---
+++ packages/frint-react-router/src/Outlet.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { observe } from 'frint-react';
+
+export default class Outlet extends Component {
+  constructor() {}
+
+  componentDidMount() {
+
+  }
+
+  render() {
+
+  }
+}
---
+++ packages/frint-react-router/src/index.js
@@ -0,0 +1,5 @@
+import Outlet from './Outlet';
+
+export default {
+  Outlet,
+};

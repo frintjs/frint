6ea956227740c04955350105a43e397367805558
jbaudin <jbaudin@travix.com>
2017-04-05 16:35:54 +0200

    unit test for the article

---
+++ examples/kitchensink/app-counter/test/components/Root.spec.js
@@ -0,0 +1,81 @@
+/* eslint-disable import/no-extraneous-dependencies, func-names, react/react-in-jsx-scope */
+/* globals BehaviorSubject, describe, document, it, sinon, expect, mount */
+import { createApp } from 'frint';
+import {
+  getMountableComponent,
+  RegionService,
+} from 'frint-react';
+import { createStore } from 'frint-store';
+
+import Root from '../../components/Root';
+import rootReducer from '../../reducers';
+
+describe('app-counter -> root component', function () {
+  it('render', function () {
+    const foo = {
+      getAppName: () => 'foo',
+    };
+
+    const bar = {
+      getAppName: () => 'bar',
+    };
+
+    const spyFoo = sinon.spy(foo, 'getAppName');
+    const spyBar = sinon.spy(bar, 'getAppName');
+
+    const App = createApp({
+      name: 'MyApp',
+      providers: [
+        {
+          name: 'component',
+          useValue: Root,
+        },
+        {
+          name: 'foo',
+          useValue: foo,
+        },
+        {
+          name: 'bar',
+          useValue: bar,
+          cascade: true,
+          scoped: true,
+        },
+        {
+          name: 'baz',
+          useValue: 'baz',
+          cascade: false, // will not be shared with children at all
+        },
+        {
+          name: 'store',
+          useFactory: ({ app }) => {
+            const Store = createStore({
+              initialState: {
+                counter: {
+                  value: 5,
+                },
+                color: {
+                  value: '#000000',
+                },
+              },
+              reducer: rootReducer,
+              thunkArgument: { app },
+            });
+            return new Store();
+          },
+        },
+        {
+          name: 'region',
+          useClass: RegionService,
+        },
+      ],
+    });
+
+    const Component = getMountableComponent(new App());
+
+    const comp = mount(<Component />);
+    expect(comp.contains(<strong>Services:</strong>)).to.equal(true);
+    expect(spyFoo.calledOnce).to.equal(true);
+    expect(spyBar.calledOnce).to.equal(true);
+  });
+});
+

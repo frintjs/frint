6dab392d1a29d2f068433a4321f5098a66d782be
jbaudin <jbaudin@travix.com>
2017-04-25 16:09:59 +0200

    typings for frint

---
+++ packages/frint-react/index.d.ts
@@ -0,0 +1,51 @@
+import { App } from 'frint';
+import { BehaviorSubject, Observale } from 'rxjs';
+
+export class Streamer {
+  constructor(...args: any[]);
+
+  set(value: any, ...args: any[]) : Streamer;
+
+  setKey(key: string, value: any): Streamer;
+
+  setPlainObject(obj: any): Streamer;
+
+  setObservable(obj$: Observale<any>, mapper: Array<(item: any, index?: number) => void>): Streamer;
+
+  setDispatch(actions: {[name: string]: (...args:any[]) => void}): Streamer;
+
+  get$(): Observable<any>;
+}
+
+export class RegionService {
+  constructor();
+
+  emit() : void;
+
+  getProps$(): BehaviorSubject<any>;
+
+  getData$(): Observale<any>;
+}
+
+export class Provider extends React.Component {
+  propTypes: React.PropTypes = {
+    app: React.PropTypes.object.isRequired,
+    children: React.PropTypes.element.isRequired,
+  };
+
+  childContextTypes: React.PropTypes = {
+    app: React.PropTypes.object.isRequired,
+  };
+
+  getChildContext(): { app: App };
+
+  constructor(props: React.PropTypes, context: any);
+
+  render() : React.ReactElement<any>;
+}
+
+export function streamProps(...args: any[]): Streamer;
+export function isObservable(obj: any): boolean;
+export function render(app: App, node: Element): void | Element | React.Component<any, React.ComponentState>;
+export function getMountableComponent(app: App): () => React.Component | Element;
+export function observe(fn: (app: App) => Observale<any>): () => React.Component | Element;

1c6e84b05e2bb070fec8bbf5a04556a784a49c53
Marcel Miranda <m@reaktivo.com>
2017-05-26 17:46:56 +0200

    Feature #193, allow loading root and child apps asynchronously

--- examples/multiple-apps/core/index.js
+++ examples/multiple-apps/core/index.js
@@ -2,7 +2,14 @@ import { render } from 'frint-react';
 
 import App from './app';
 
+const apps = window.app || [];
 window.app = new App();
+
+window.app.push = function ([app, options]) {
+  window.app.registerApp(app, options);
+};
+
+apps.forEach(window.app.push);
 
 render(
   window.app,

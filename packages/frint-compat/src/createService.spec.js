45dba4b81ef2cc814fb9bad89a5ff815240f776e
Fahad Ibnay Heylaal <fahad19@gmail.com>
2017-06-22 13:53:09 +0200

    fix for chai

--- packages/frint-compat/src/createService.spec.js
+++ packages/frint-compat/src/createService.spec.js
@@ -35,8 +35,13 @@ describe('frint-compat › createService', () => {
     expect(mySpec.initialize).to.be.callCount(1);
   });
 
-  it('must contain the functions passed in the spec', () => {
-    expect(myServiceInstance).to.include.all.keys('initialize', 'customFunction1', 'customFunction2', 'returnsThis');
+  it.only('must contain the functions passed in the spec', () => {
+    expect(Object.keys(myServiceInstance)).to.include(
+      'initialize',
+      'customFunction1',
+      'customFunction2',
+      'returnsThis'
+    );
 
     expect(myServiceInstance.customFunction1()).to.be.equal('value1');
     expect(myServiceInstance.customFunction2()).to.be.equal('value2');

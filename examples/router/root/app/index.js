3dd3f1a0ce1ab8fd6ddcf1ebc8dfcd34a491d098
Fahad Ibnay Heylaal <fahad19@gmail.com>
2017-07-04 20:33:26 +0200

    examples: static routes (expected API usage).

--- examples/router/root/app/index.js
+++ examples/router/root/app/index.js
@@ -1,13 +1,62 @@
 import { createApp } from 'frint';
+import HashRouterService from 'frint-router/HashRouterService';
 
-import RootComponent from '../components/Root';
+import Root from '../components/Root';
+
+import HomePage from '../components/HomePage';
+import AboutPage from '../components/AboutPage';
+
+import MessagesApp from '../../app-messages/app';
+import TodosApp from '../../app-todos/app';
 
 export default createApp({
   name: 'KitchensinkApp',
   providers: [
+    // root component
     {
       name: 'component',
-      useValue: RootComponent
+      useValue: Root
     },
+
+    // router service
+    // (required)
+    {
+      name: 'router',
+      useFactory: function () {
+        return new HashRouterService();
+      },
+    },
+
+    // static routes configuration (pre-defined)
+    // (optional)
+    {
+      name: 'routes',
+      useValue: [
+        // sync component
+        {
+          path: '/',
+          component: HomePage,
+          full: true, // matches full path
+        },
+
+        // async component
+        {
+          path: '/about',
+          getComponent: cb => cb(null, AboutPage), // will bring its own dynamic child routes
+        },
+
+        // sync app
+        {
+          path: '/messages',
+          App: MessagesApp, // will bring its own dynamic child routes
+        },
+
+        // async app
+        {
+          path: '/todos',
+          getApp: cb => cb(null, TodosApp), // will brings own own dynamic child routes
+        }
+      ]
+    }
   ],
 });

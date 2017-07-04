6b23a231db8922033666529de74f44252a4e597a
Fahad Ibnay Heylaal <fahad19@gmail.com>
2017-07-04 19:50:36 +0200

    examples: top nav with links.

--- examples/router/root/components/Root.js
+++ examples/router/root/components/Root.js
@@ -1,9 +1,21 @@
 import React from 'react';
 
+import TopNav from './TopNav';
+
 export default function Root() {
   return (
     <div>
-      <p>Hello World!</p>
+      <TopNav />
+
+      <section className="section">
+        <div className="container">
+          <div className="columns">
+            <div className="column content">
+              <p>Hello World</p>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }

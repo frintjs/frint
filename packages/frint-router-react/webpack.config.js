c2190962aac74e1109ec10d9829bb2810858946d
Fahad Ibnay Heylaal <fahad19@gmail.com>
2017-07-04 15:27:44 +0200

    frint-router-react: base package.

---
+++ packages/frint-router-react/webpack.config.js
@@ -0,0 +1,45 @@
+var webpack = require('webpack');
+
+var minify = process.env.DIST_MIN;
+var plugins = !minify
+  ? []
+  : [
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false,
+        drop_console: false
+      }
+    })
+  ];
+var filename = !minify
+  ? 'frint-router-react.js'
+  : 'frint-router-react.min.js';
+
+module.exports = {
+  entry: __dirname + '/src',
+  output: {
+    path: __dirname + '/dist',
+    filename: filename,
+    libraryTarget: 'this',
+    library: 'FrintRouterReact'
+  },
+  externals: {
+    'lodash': '_',
+  },
+  target: 'web',
+  plugins: plugins,
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        loader: 'babel-loader',
+        query: {
+          presets: [
+            'travix'
+          ]
+        }
+      }
+    ]
+  }
+};

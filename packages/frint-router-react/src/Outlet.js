f34355e5c44f4a5cddddd520bdcc05a3361d631a
Fahad Ibnay Heylaal <fahad19@gmail.com>
2017-07-05 18:13:24 +0200

    frint-router-react: initial commit for components.

---
+++ packages/frint-router-react/src/Outlet.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import Route from './Route';
+
+export default class Outlet extends React.Component {
+  static propTypes = {
+    children: PropTypes.node,
+  };
+
+  constructor(...args) {
+    super(...args);
+
+    this.state = {
+      routes: [],
+    };
+  }
+
+  componentDidMount() {
+    if (this.props.routes) {
+      // passed as configuration via props
+      this.state.routes = this.props.routes;
+    } else {
+      // check if available as config from provider
+      const routesConfig = this.context.app.get('routes');
+
+      if (Array.isArray(routesConfig)) {
+        this.state.routes = routesConfig;
+      }
+    }
+  }
+
+  render() {
+    if (this.props.children) {
+      return this.props.children;
+    }
+
+    if (this.state.routes.length === 0) {
+      return null;
+    }
+
+    return this.state.routes.map(function (routeProps, index) {
+      // @TODO: handle routeProps.children (if any)
+      return (
+        <Route
+          key={index}
+          {...routeProps}
+        />
+      );
+    });
+  }
+}
--- packages/frint-router-react/src/index.js
+++ packages/frint-router-react/src/index.js
@@ -1,1 +1,7 @@
-export default {};
+import Outlet from './Outlet';
+import Route from './Route';
+
+export default {
+  Outlet,
+  Route,
+};

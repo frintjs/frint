735808bf3e20f53f5325bc28b838e635826ad26b
Fahad Ibnay Heylaal <fahad19@gmail.com>
2017-07-06 18:09:40 +0200

    examples: updates to bundle unpublished packages.

--- examples/router/vendors/index.js
+++ examples/router/vendors/index.js
@@ -4,3 +4,6 @@ window.Rx = require('rxjs');
 window.Frint = require('frint');
 window.FrintStore = require('frint-store');
 window.FrintReact = require('frint-react');
+
+window.FrintRouter = require('../../../packages/frint-router');
+window.FrintRouterReact = require('../../../packages/frint-router-react');

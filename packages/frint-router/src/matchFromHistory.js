de1cda9d3bed2a19717f971c619178ba50099d16
Fahad Ibnay Heylaal <fahad19@gmail.com>
2017-07-10 16:35:23 +0200

    frint-router: matchFromHistory util with tests.

---
+++ packages/frint-router/src/matchFromHistory.js
@@ -0,0 +1,32 @@
+import _ from 'lodash';
+import pathToRegexp from 'path-to-regexp';
+
+export default function matchFromHistory(pattern, history, options = {}) {
+  const keys = [];
+  const re = pathToRegexp(pattern, keys, {
+    end: options.exact ? true : false,
+  });
+
+  const matched = re.exec(history.location.pathname);
+
+  if (!matched) {
+    return null;
+  }
+
+  const url = matched[0];
+  const restValues = _.tail(matched);
+  const keyNames = keys.map(k => k.name);
+  const isExact = (url === history.location.pathname);
+
+  if (options.exact && !isExact) {
+    return null;
+  }
+
+  const params = _.zipObject(keyNames, restValues);
+
+  return {
+    url,
+    isExact,
+    params,
+  };
+}

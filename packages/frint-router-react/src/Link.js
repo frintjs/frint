461821cf4c9a20ab113b697de1985bc041439200
Slava Zhuravski <szhuravski@travix.com>
2017-07-20 14:37:57 +0200

    Add eslint exception for import/no-extraneous-dependencies for dependencies from root package.json

--- packages/frint-router-react/src/Link.js
+++ packages/frint-router-react/src/Link.js
@@ -1,7 +1,5 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-
-// TODO check package(s).json
+import React from 'react'; // eslint-disable-line import/no-extraneous-dependencies
+import PropTypes from 'prop-types'; // eslint-disable-line import/no-extraneous-dependencies
 
 class Link extends React.Component {
   static contextTypes = {

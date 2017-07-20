153a0ca7cc3c57a6df5793201630814cc0f1b929
Fahad Ibnay Heylaal <fahad19@gmail.com>
2017-07-20 14:15:43 +0200

    frint-router-react: drop Outlet.

--- examples/router/root/app/index.js
+++ examples/router/root/app/index.js
@@ -3,12 +3,6 @@ import HashRouterService from 'frint-router/HashRouterService';
 
 import Root from '../components/Root';
 
-import HomePage from '../components/HomePage';
-import AboutPage from '../components/AboutPage';
-
-import ContactApp from '../../app-contact/app';
-import ServicesApp from '../../app-services/app';
-
 export default createApp({
   name: 'RouterApp',
   providers: [
@@ -27,37 +21,5 @@ export default createApp({
       },
       cascade: true,
     },
-
-    // static routes configuration (pre-defined)
-    // (optional)
-    {
-      name: 'routes',
-      useValue: [
-        // sync component
-        {
-          path: '/',
-          component: HomePage,
-          exact: true, // matches full path
-        },
-
-        // async component
-        {
-          path: '/about',
-          getComponent: cb => cb(null, AboutPage), // will bring its own dynamic child routes
-        },
-
-        // // sync app
-        {
-          path: '/contact',
-          app: ContactApp,
-        },
-
-        // // async app
-        {
-          path: '/services',
-          getApp: cb => cb(null, ServicesApp),
-        },
-      ]
-    }
   ],
 });
--- packages/frint-router-react/src/index.js
+++ packages/frint-router-react/src/index.js
@@ -1,10 +1,8 @@
-import Outlet from './Outlet';
 import Route from './Route';
 import Link from './Link';
 import Switch from './Switch';
 
 export default {
-  Outlet,
   Route,
   Link,
   Switch,

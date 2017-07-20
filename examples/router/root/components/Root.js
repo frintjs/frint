8b560e917fe00db5cb7ba118deb6504d1b9edcde
Fahad Ibnay Heylaal <fahad19@gmail.com>
2017-07-20 14:15:43 +0200

    frint-router-react: drop Outlet.

--- examples/router/root/components/Root.js
+++ examples/router/root/components/Root.js
@@ -1,14 +1,23 @@
 import React from 'react';
-import { Outlet } from 'frint-router-react';
+import { Switch, Route } from 'frint-router-react';
 
 import TopNav from './TopNav';
+
+import HomePage from './HomePage';
+import AboutPage from './AboutPage';
+
+import ContactApp from '../../app-contact/app';
+import ServicesApp from '../../app-services/app';
 
 export default function Root() {
   return (
     <div>
       <TopNav />
 
-      <Outlet />
+      <Route path="/" component={HomePage} exact />
+      <Route path="/about" getComponent={cb => cb(null, AboutPage)} />
+      <Route path="/contact" app={ContactApp} />
+      <Route path="/services" getApp={cb => cb(null, SevicesApp)} />
     </div>
   );
 }

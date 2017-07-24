f2e22da1e221c3a75421eb7fdac1934e5991cf04
Fahad Ibnay Heylaal <fahad19@gmail.com>
2017-07-24 17:31:20 +0200

    frint-router: tests.

--- packages/frint-router/src/makeRouterService.spec.js
+++ packages/frint-router/src/makeRouterService.spec.js
@@ -63,13 +63,154 @@ describe('frint-router › makeRouterService', function () {
     it('matches pattern against history synchronously', function () {
       const router = new MemoryRouterService();
 
+      // initial
       expect(router.getMatch(
         '/about',
         router.getHistory()
       )).to.equal(null);
 
-      router.push('/')
-      // expect(router.)
+      // after navigating to /about
+      router.push('/about');
+      expect(router.getMatch(
+        '/about',
+        router.getHistory()
+      )).to.deep.equal({
+        isExact: true,
+        params: {},
+        url: '/about',
+      });
+
+      // after navigating back to /
+      router.push('/');
+      expect(router.getMatch(
+        '/about',
+        router.getHistory()
+      )).to.equal(null);
+      expect(router.getLocation().pathname).to.equal('/');
+    });
+
+    it('matches pattern asynchronously', function (done) {
+      const router = new MemoryRouterService();
+
+      router.getMatch$('/about')
+        .take(3)
+        .scan(
+          (matches, currentMatch) => matches.concat([currentMatch]),
+          []
+        )
+        .last()
+        .subscribe(function (matches) {
+          expect(matches).to.deep.equal([
+            // on load
+            null,
+
+            // first push
+            {
+              url: '/about',
+              isExact: true,
+              params: {},
+            },
+
+            // second push
+            null,
+          ]);
+
+          done();
+        });
+
+      router.push('/about');
+      router.push('/');
+    });
+
+    it('destroys listener', function (done) {
+      const router = new MemoryRouterService();
+
+      const entries = [];
+
+      router.getHistory$().subscribe({
+        next(entry) {
+          entries.push(entry);
+        },
+        complete() {
+          expect(entries.length).to.equal(1);
+
+          done();
+        },
+      });
+
+      router.destroy();
+    });
+
+    it('goes to nth entry in history', function (done) {
+      const router = new MemoryRouterService();
+
+      router.push('/about');
+      router.push('/about/hermione');
+      router.go(-1);
+
+      router.getLocation$()
+        .subscribe(function (location) {
+          expect(location.pathname).to.equal('/about');
+
+          done();
+        });
+    });
+
+    it('pushes history', function (done) {
+      const router = new MemoryRouterService();
+
+      router.push('/about');
+
+      router.getLocation$()
+        .subscribe(function (location) {
+          expect(location.pathname).to.equal('/about');
+
+          done();
+        });
+    });
+
+    it('replaces history', function (done) {
+      const router = new MemoryRouterService();
+
+      router.replace('/about');
+
+      router.getLocation$()
+        .subscribe(function (location) {
+          expect(location.pathname).to.equal('/about');
+
+          done();
+        });
+    });
+
+    it('goes back in history', function (done) {
+      const router = new MemoryRouterService();
+
+      router.push('/about');
+      router.push('/about/hermione');
+      router.goBack();
+
+      router.getLocation$()
+        .subscribe(function (location) {
+          expect(location.pathname).to.equal('/about');
+
+          done();
+        });
+    });
+
+    it('goes forward in history', function (done) {
+      const router = new MemoryRouterService();
+
+      router.push('/about');
+      router.push('/about/hermione');
+      router.goBack();
+      router.goForward();
+
+      router.getLocation$()
+        .subscribe(function (location) {
+          expect(location.pathname).to.equal('/about/hermione');
+
+          done();
+        });
     });
   });
 });

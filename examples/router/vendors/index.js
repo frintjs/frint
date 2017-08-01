4c55b52e704b4970d9cadce09fbbff905ae067b7
Fahad Ibnay Heylaal <fahad19@gmail.com>
2017-08-01 10:08:20 +0200

    examples: require packages from local node_modules.

--- examples/router/vendors/index.js
+++ examples/router/vendors/index.js
@@ -4,6 +4,5 @@ window.Rx = require('rxjs');
 window.Frint = require('frint');
 window.FrintStore = require('frint-store');
 window.FrintReact = require('frint-react');
-
-window.FrintRouter = require('../../../packages/frint-router');
-window.FrintRouterReact = require('../../../packages/frint-router-react');
+window.FrintRouter = require('frint-router');
+window.FrintRouterReact = require('frint-router-react');

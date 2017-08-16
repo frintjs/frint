9c1b6608317775abb646b56c036b34294fcfc3a8
Fahad Ibnay Heylaal <fahad19@gmail.com>
2017-08-16 18:29:22 +0200

    frint-store: feature implementation.

--- packages/frint-store/src/combineEpics.js
+++ packages/frint-store/src/combineEpics.js
@@ -1,10 +1,10 @@
 import { Observable } from 'rxjs';
 
 export default function combineEpics(...epics) {
-  return function (...args) {
+  return function rootEpic(...args) {
     const toMerge = epics
       .map(epic => epic(...args));
 
     return Observable.merge(...toMerge);
-  }
+  };
 }

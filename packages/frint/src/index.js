7c712125e9253511ea51701888e48d76f8e2dea2
Marcel Miranda <m@reaktivo.com>
2017-08-16 14:49:47 +0200

    Added Frint version number to exportable

--- packages/frint/src/index.js
+++ packages/frint/src/index.js
@@ -1,9 +1,11 @@
 import App from './App';
 import createApp from './createApp';
+import { version } from '../package';
 
 const Frint = {
   App,
   createApp,
+  version,
 };
 
 export default Frint;

f62dda289b929a949768b25f6ed23e1a1db589a4
Fahad Ibnay Heylaal <fahad19@gmail.com>
2017-09-25 15:56:25 +0200

    deprecations: show warnings when creating Store and Model classes, without waiting for instantiations.

--- packages/frint-model/src/createModel.js
+++ packages/frint-model/src/createModel.js
@@ -4,6 +4,8 @@ import _ from 'lodash';
 import BaseModel from './Model';
 
 export default function createModel(extend = {}) {
+  console.warn('[DEPRECATED] frint-model has been deprecated, use frint-data instead');
+
   class Model extends BaseModel {
     constructor(...args) {
       super(...args);
--- packages/frint-store/src/createStore.js
+++ packages/frint-store/src/createStore.js
@@ -3,6 +3,11 @@ import _ from 'lodash';
 import BaseStore from './Store';
 
 export default function createStore(options = {}) {
+  if (options.thunkArgument) {
+    console.warn('[DEPRECATED] Use `deps` instead of `thunkArgument` option');
+    options.deps = options.thunkArgument;
+  }
+
   class Store extends BaseStore {
     constructor(opts = {}) {
       super(_.merge(

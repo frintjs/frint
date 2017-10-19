e09ddea86fb5268816911bbba67d02773268b495
Andrea Ascari <dev.ascariandrea@gmail.com>
2017-10-19 14:02:45 +0200

    frint-config: exports (#346)
    * granular exports for frint-config externals
    
    * Added 'Externals' suffix to exported memebers of frint-config
    
    * Fixed typo due to my rush

--- packages/frint-config/src/externals.js
+++ packages/frint-config/src/externals.js
@@ -3,59 +3,120 @@
  * Webpack's `externals` equivalent object,
  * listing dependencies that Frint packages use.
  */
-import webpackRxjsExternals from 'webpack-rxjs-externals';
+import webpackRxJsExternals from 'webpack-rxjs-externals';
+
+export const rxJs = webpackRxJsExternals();
+
+export const lodash = function (context, request, callback) {
+  if (request.startsWith('lodash/')) {
+    const subModule = request.split('/')[1];
+
+    return callback(null, {
+      root: ['_', subModule],
+      commonjs: request,
+      commonjs2: request,
+      amd: request,
+    });
+  }
+
+  return callback();
+};
+
+// full imports
+export const thirdParties = [{
+  'lodash': {
+    root: '_',
+    commonjs: 'lodash',
+    commonjs2: 'lodash',
+    amd: 'lodash',
+  },
+}, {
+  'rxjs': {
+    root: 'Rx',
+    commonjs: 'rxjs',
+    commonjs2: 'rxjs',
+    amd: 'rxjs',
+  },
+}, {
+  'react': {
+    root: 'React',
+    commonjs: 'react',
+    commonjs2: 'react',
+    amd: 'react',
+  },
+}, {
+  'react-dom': {
+    root: 'ReactDOM',
+    commonjs: 'react-dom',
+    commonjs2: 'react-dom',
+    amd: 'react-dom'
+  }
+}, {
+  'prop-types': {
+    root: 'PropTypes',
+    commonjs: 'prop-types',
+    commonjs2: 'prop-types',
+    amd: 'prop-types',
+  }
+}];
+
+export const frint = [{
+  'frint': {
+    root: 'Frint',
+    commonjs: 'frint',
+    commonjs2: 'frint',
+    amd: 'frint'
+  },
+}, {
+  'frint-store': {
+    root: 'FrintStore',
+    commonjs: 'frint-store',
+    commonjs2: 'frint-store',
+    amd: 'frint-store'
+  },
+}, {
+  'frint-model': {
+    root: 'FrintModel',
+    commonjs: 'frint-model',
+    commonjs2: 'frint-model',
+    amd: 'frint-model'
+  },
+}, {
+  'frint-data': {
+    root: 'FrintData',
+    commonjs: 'frint-data',
+    commonjs2: 'frint-data',
+    amd: 'frint-data'
+  },
+}, {
+  'frint-react': {
+    root: 'FrintReact',
+    commonjs: 'frint-react',
+    commonjs2: 'frint-react',
+    amd: 'frint-react'
+  },
+}, {
+  'frint-router': {
+    root: 'FrintRouter',
+    commonjs: 'frint-router',
+    commonjs2: 'frint-router',
+    amd: 'frint-router'
+  },
+}, {
+  'frint-router-react': {
+    root: 'FrintRouterReact',
+    commonjs: 'frint-router-react',
+    commonjs2: 'frint-router-react',
+    amd: 'frint-router-react'
+  }
+}];
 
 export default [
   // rxjs/*
-  webpackRxjsExternals(),
+  rxJs,
 
   // lodash/*
-  function (context, request, callback) {
-    if (request.startsWith('lodash/')) {
-      const subModule = request.split('/')[1];
+  lodash,
 
-      return callback(null, {
-        root: ['_', subModule],
-        commonjs: request,
-        commonjs2: request,
-        amd: request,
-      });
-    }
-
-    return callback();
-  },
-
-  // full imports
-  {
-    'lodash': {
-      root: '_',
-      commonjs: 'lodash',
-      commonjs2: 'lodash',
-      amd: 'lodash',
-    },
-    'rxjs': {
-      root: 'Rx',
-      commonjs: 'rxjs',
-      commonjs2: 'rxjs',
-      amd: 'rxjs',
-    },
-    'react': {
-      root: 'React',
-      commonjs: 'react',
-      commonjs2: 'react',
-      amd: 'react',
-    },
-    'react-dom': {
-      root: 'ReactDOM',
-      commonjs: 'react-dom',
-      commonjs2: 'react-dom',
-      amd: 'react-dom',
-    },
-    'prop-types': {
-      root: 'PropTypes',
-      commonjs: 'prop-types',
-      commonjs2: 'prop-types',
-      amd: 'prop-types',
-    },
-  },
+  ...thirdParties
 ];
--- packages/frint-config/src/index.js
+++ packages/frint-config/src/index.js
@@ -1,5 +1,14 @@
-import externals from './externals';
+import externals, {
+  frint,
+  lodash,
+  thirdParties,
+  rxJs
+} from './externals';
 
 export default {
   externals,
+  frintExternals: frint,
+  lodashExternals: lodash,
+  thirdPartiesExternals: thirdParties,
+  rxJsExternals: rxJs
 };

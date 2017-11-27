8be6b9036166fa7c3dcd089a173b524ede643267
Fahad Ibnay Heylaal <fahad19@gmail.com>
2017-11-27 14:16:11 +0100

    frint: handle App lifecycle without overriding options

--- packages/frint/src/createApp.js
+++ packages/frint/src/createApp.js
@@ -2,13 +2,33 @@ import merge from 'lodash/merge';
 
 import BaseApp from './App';
 
+function mergeOptions(createAppOptions, constructorOptions) {
+  const mergedOptions = merge({}, createAppOptions, constructorOptions);
+
+  // keep lifecycle methods from both
+  // `createApp(options)` and `new App(options)`
+  [
+    'initialize',
+    'beforeDestroy',
+  ].forEach((cbName) => {
+    if (
+      typeof createAppOptions[cbName] === 'function' &&
+      typeof constructorOptions[cbName] === 'function'
+    ) {
+      mergedOptions[cbName] = function lifecycleCb() {
+        createAppOptions[cbName]();
+        constructorOptions[cbName]();
+      };
+    }
+  });
+
+  return mergedOptions;
+}
+
 export default function createApp(options = {}) {
   class App extends BaseApp {
     constructor(opts = {}) {
-      super(merge(
-        options,
-        opts
-      ));
+      super(mergeOptions(options, opts));
     }
   }
 

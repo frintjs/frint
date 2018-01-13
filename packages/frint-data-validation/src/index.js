0d7bf2827dcaf0867cd20b9b55402a46ab07dd04
Fahad Ibnay Heylaal <fahad19@gmail.com>
2018-01-13 21:56:09 +0100

    frint-data-validation: validate function (wip).

--- packages/frint-data-validation/src/index.js
+++ packages/frint-data-validation/src/index.js
@@ -1,1 +1,5 @@
-export default {};
+import validate from './validate';
+
+export default {
+  validate,
+};
---
+++ packages/frint-data-validation/src/validate.js
@@ -0,0 +1,68 @@
+function extractRules(model, givenRules) {
+  // check if rules are passed manually
+  if (givenRules) {
+    return givenRules;
+  }
+
+  // check if Model class had rules defined statically
+  if (
+    !model ||
+    !model.__proto__ ||
+    !model.__proto__.constructor ||
+    !model.__proto__.constructor.validationRules
+  ) {
+    return [];
+  }
+
+  const rules = model.__proto__.constructor.validationRules;
+
+  // rules not defined properly
+  if (!Array.isArray(rules)) {
+    throw Error(`Validation rules not defined correctly in Model`);
+  }
+
+  return rules;
+}
+
+export default function validate(model, givenRules = null, opts = {}) {
+  const rules = extractRules(model, givenRules);
+
+  if (rules.length === 0) {
+    // nothing to validate
+    return [];
+  }
+
+  const options = {
+    // @TODO: handle async later
+    // async: false,
+    ...opts,
+  };
+
+  const result = rules
+    .map(function (r) {
+      const {
+        rule,
+        message,
+        name
+      } = r;
+
+      const output = rule(model);
+
+      if (output === false) {
+        return {
+          name,
+          message,
+        };
+      }
+
+      return null;
+    })
+    .filter(r => r);
+
+  // @TODO: handle async later
+  // if (options.async) {
+  //   // @TODO: handle async validations in `result`
+  // }
+
+  return result;
+}

f613f6f5b952826b8cfbf5b89315aaf60e0eee4c
Slava Zhuravski <szhuravski@travix.com>
2018-03-22 11:09:41 +0100

    Make options optional in createContainer() definition

--- packages/frint-di/index.d.ts
+++ packages/frint-di/index.d.ts
@@ -24,6 +24,6 @@ export interface Constructor<T> {
   new(): T;
 }
 
-export function createContainer(providers: Provider[], options: ContainerOptions): Constructor<Container>;
+export function createContainer(providers: Provider[], options?: ContainerOptions): Constructor<Container>;
 
 export function resolveContainer<T>(Container: Constructor<T>): T;
